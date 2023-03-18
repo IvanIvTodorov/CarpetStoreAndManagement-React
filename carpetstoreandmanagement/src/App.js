f4b9080518ddfa0fd3f3ec24890ccff9e9fb3629
Ivan Todorov <iwan.todorow@gmail.com>
2023-03-18 10:10:55 +0200

    Add footer component

--- carpetstoreandmanagement/src/App.js
+++ carpetstoreandmanagement/src/App.js
@@ -2,17 +2,19 @@ import { Routes, Route } from "react-router-dom"
 
 import 'bootstrap/dist/css/bootstrap.css';
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
+      <main style={{ minHeight: "517px" }}>
         <Routes>
 
 
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ carpetstoreandmanagement/src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import style from './Footer.Module.css'
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div className="bottom section-padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                            <div className="copyright">
+                                <p>
+                                    © <span>2023</span>{" "}
+                                    CarpetStoreAndManagement
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
