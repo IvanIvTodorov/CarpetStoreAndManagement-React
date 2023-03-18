7f0e606a56c67def38f796ecf39abad88425ec5c
Ivan Todorov <iwan.todorow@gmail.com>
2023-03-18 11:29:41 +0200

    Add products component

---
+++ carpetstoreandmanagement/src/components/Products/Products.js
@@ -0,0 +1,110 @@
+import style from './Products.Module.css'
+
+export const Products = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-3 col-sm-6">
+                    <div className="product-grid2">
+                        <div className="product-image2">
+                            <a href="#">
+                                <img className="pic-1" style={{width: '350px', height: '400px'}} src="https://picsum.photos/200/300" />
+                                <img className="pic-2" style={{width: '350px', height: '400px'}} src="https://picsum.photos/200/300" />
+
+                            </a>
+                            <a className="add-to-cart" href="">
+                                Add to cart
+                            </a>
+                        </div>
+                        <div className="product-content">
+                            <h3 className="title">
+                                <a href="#">Women's Designer Top</a>
+                            </h3>
+                            <span className="price">$599.99</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <div className="product-grid2">
+                        <div className="product-image2">
+                            <a href="#">
+                                <img className="pic-1" style={{width: '350px', height: '400px'}} src="https://picsum.photos/200/300" />
+                                <img className="pic-2" style={{width: '350px', height: '400px'}} src="https://picsum.photos/200/300" />
+
+                            </a>
+                            <a className="add-to-cart" href="">
+                                Add to cart
+                            </a>
+                        </div>
+                        <div className="product-content">
+                            <h3 className="title">
+                                <a href="#">Women's Designer Top</a>
+                            </h3>
+                            <span className="price">$599.99</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <div className="product-grid2">
+                        <div className="product-image2">
+                            <a href="#">
+                                <img className="pic-1" style={{width: '350px', height: '400px'}} src="https://www.e-kilimi.com/uploads/com_article/gallery/9112140612e7c36c7acfa00005b5563767c24a9a.jpg" />
+                                <img className="pic-2" style={{width: '350px', height: '400px'}} src="https://www.e-kilimi.com/uploads/com_article/gallery/9112140612e7c36c7acfa00005b5563767c24a9a.jpg" />
+
+                            </a>
+                            <a className="add-to-cart" href="">
+                                Add to cart
+                            </a>
+                        </div>
+                        <div className="product-content">
+                            <h3 className="title">
+                                <a href="#">Women's Designer Top</a>
+                            </h3>
+                            <span className="price">$599.99</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <div className="product-grid2">
+                        <div className="product-image2">
+                            <a href="#">
+                                <img className="pic-1" style={{width: '350px', height: '400px'}} src="https://thumbs.dreamstime.com/b/carpet-20820761.jpg" />
+                                <img className="pic-2" style={{width: '350px', height: '400px'}} src="https://thumbs.dreamstime.com/b/carpet-20820761.jpg" />
+
+                            </a>
+                            <a className="add-to-cart" href="">
+                                Add to cart
+                            </a>
+                        </div>
+                        <div className="product-content">
+                            <h3 className="title">
+                                <a href="#">Women's Designer Top</a>
+                            </h3>
+                            <span className="price">$599.99</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-3 col-sm-6">
+                    <div className="product-grid2">
+                        <div className="product-image2">
+                            <a href="#">
+                                <img className="pic-1" style={{width: '350px', height: '400px'}} src="https://picsum.photos/200/300" />
+                                <img className="pic-2" style={{width: '350px', height: '400px'}} src="https://picsum.photos/200/300" />
+
+                            </a>
+                            <a className="add-to-cart" href="">
+                                Add to cart
+                            </a>
+                        </div>
+                        <div className="product-content">
+                            <h3 className="title">
+                                <a href="#">Women's Designer Top</a>
+                            </h3>
+                            <span className="price">$599.99</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
